69b29548ccd29a6a330731c90926ec848d5a43e9
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-01-25 21:14:17 +0530

    added login

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -22,7 +22,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
 
-  // Effect to fetch user from localStorage
+ 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
