772115c2204ba6b56048e0fdf4eee242e15d33c8
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-02-13 23:42:50 +0530

    Protecting url path done

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,15 +7,31 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =
   const { user} = useAuth();
   const location = useLocation();
 
-  console.log(user?.type)
    
   if (!user && !location.pathname.includes("/login")){
     return <Navigate to="/login" replace />;
   } 
   
-if ( user?.type === "admin" && !location.pathname.includes("/admin")){
-  return <Navigate to="/admin" replace />;
-}
+  if ( user?.type === "admin" && !location.pathname.includes("/admin")){
+    return <Navigate to="/admin" replace />;
+  }
+  if ( user?.type === "attendance-officer" && !location.pathname.includes("/attendance-officer")){
+    return <Navigate to="/attendance-officer" replace />;
+  }
+  
+  if ( user?.type === "parent" && !location.pathname.includes("/parent")){
+    return <Navigate to="/parent" replace />;
+  }
+  
+  if ( user?.type === "school-bus" && !location.pathname.includes("/school-bus")){
+    return <Navigate to="/school-bus" replace />;
+  }
+  
+  if ( user?.type === "teacher" && !location.pathname.includes("/teacher")){
+    return <Navigate to="/teacher" replace />;
+  }
+  
+  
 
   return (
     <div className="min-h-screen flex flex-col">
